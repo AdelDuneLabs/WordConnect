0716fe42c9befc7e5f6a0ea256686f2d6dc2e3c3
Tarik <tarik.yazici.790@gmail.com>
2023-05-14 13:22:34 +0300

    save

--- Assets/Tarek/Scripts/LeaderboardItem.cs
+++ Assets/Tarek/Scripts/LeaderboardItem.cs
@@ -2,23 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeaderboardItem : MonoBehaviour
 {
     [SerializeField] TMP_Text rank_Text, name_Text, score_Text;
+    [SerializeField] Color firstPlace_Color, otherPlaces_Color;
+    [SerializeField] Image first_Image, scnd_Image, thrd_Image;
+    [SerializeField] Image BG;
     public void Set(int rank, string name, int score)
     {
+        BG.color = rank == 1 ? firstPlace_Color : otherPlaces_Color;
+        first_Image.gameObject.SetActive(false);
+        scnd_Image.gameObject.SetActive(false);
+        thrd_Image.gameObject.SetActive(false);
         string rankText;
         switch (rank)
         {
             case 1:
                 rankText = "1st";
+                first_Image.gameObject.SetActive(true);
                 break;
             case 2:
                 rankText = "2nd";
+                scnd_Image.gameObject.SetActive(true);
                 break;
             case 3:
                 rankText = "3rd";
+                thrd_Image.gameObject.SetActive(true);
                 break;
             default:
                 rankText = rank + "th";
